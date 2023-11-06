60dafea6ee76d3e5968b5d2dfffce1791999f80a
Vedant Nimbarte <nimbartevedant@gmail.com>
2023-11-06 22:54:59 +0530

    Added kafka config object

---
+++ src/kafka.config.ts
@@ -0,0 +1,7 @@
+export default {
+  kafka: {
+    clientId: 'kafka-manager',
+    brokers: ['localhost:9092'], // Update with your Kafka broker address
+    topic: '<your-topic-name>', // Update with the Kafka topic you intend to use
+  },
+};
